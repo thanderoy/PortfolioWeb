1c9c5db93c205b90ab5961e872ed6535f062b621
thanderoy <roy.thande@gmail.com>
2020-09-15 10:55:47 +0300

    Fixed import os and allowed hosts

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thanderoy.com']
+ALLOWED_HOSTS = ['thanderoy.com', 'https://thande-portfolio.herokuapp.com/','127.0.0.1:8000']
 
 
 # Application definition
