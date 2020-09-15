9158b3cdbddd5b20dd51f6d17158e4c7b3c7b489
thanderoy <roy.thande@gmail.com>
2020-09-15 11:58:07 +0300

    Fix S3

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['thanderoy.com', 'thande-portfolio.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = ['thanderoy.com', 'thande-portfolio.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -123,17 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
-
-MEDIA_URL = 'https://thanderoy-portfolio.bucket.amazonaws.com/static/images/'
 STATIC_URL = 'https://thanderoy-portfolio.bucket.s3.amazonaws.com/static/'
+MEDIA_URL = 'https://thanderoy-portfolio.bucket.amazonaws.com/static/images/'
+
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 MEDIAFILES_DIRS = [
-    BASE_DIR /'static/images'
+    BASE_DIR / 'static/images'
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
