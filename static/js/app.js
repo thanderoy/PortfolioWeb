4980bf2d16b97f6ae20bba26fe06b69f5f1c7b0a
thanderoy <roy.thande@gmail.com>
2020-10-23 22:09:54 +0300

    UI updated, Email template fixed

--- static/js/app.js
+++ static/js/app.js
@@ -2,29 +2,37 @@ const hamburger = document.querySelector('.hamburger');
 const banner = document.querySelector('.banner');
 const navi = document.querySelector('.navi');
 const navLinks = document.querySelectorAll('.navlink');
+const body = document.querySelector('.body');
 let menuOpen = false;
 
 hamburger.addEventListener('click', () => {
     if (!menuOpen) {
-        hamburger.classList.add('open');
-        banner.classList.add('sidekick');
-        navi.classList.add('navi-open');
-        menuOpen = true;
+        openNav();
     } else {
-        hamburger.classList.remove('open');
-        banner.classList.remove('sidekick');
-        navi.classList.remove('navi-open');
-        menuOpen = false;
+        closeNav();
     }
 });
 
 for (const navLink of navLinks) {
     navLink.addEventListener('click', () => {
         if (menuOpen) {
-            hamburger.classList.remove('open');
-            banner.classList.remove('sidekick');
-            navi.classList.remove('navi-open');
-            menuOpen = false;
+            closeNav();
         }
     });
+}
+
+function openNav() {
+    hamburger.classList.add('open');
+    banner.classList.add('sidekick');
+    navi.classList.add('navi-open');
+    body.classList.add('lock-scroll');
+    menuOpen = true;
+}
+
+function closeNav() {
+    hamburger.classList.remove('open');
+    banner.classList.remove('sidekick');
+    navi.classList.remove('navi-open');
+    body.classList.remove('lock-scroll');
+    menuOpen = false;
 }
