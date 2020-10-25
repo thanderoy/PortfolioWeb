cb8babf4f0d407b4080cd4dbf2c7790e08ce51a0
thanderoy <roy.thande@gmail.com>
2020-10-25 23:02:23 +0300

    Modified views.py

--- P_App/views.py
+++ P_App/views.py
@@ -4,6 +4,13 @@ from django.core.mail import EmailMessage
 from django.conf import settings
 from django.template.loader import render_to_string
 
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+
+dt_string = now.strftime("%B %d, %Y %H:%M")
+
 # Create your views here.
 def index(request):
     return render(request, 'P_App/index.html')
@@ -17,6 +24,7 @@ def sendEmail(request):
             'username': request.POST['username'],
             'email': request.POST['email'],
             'message': request.POST['message'],
+            'dt_string': dt_string
         })
 
         email = EmailMessage(
