f9ca43ccf614d522f32ad58bfae983ae6a89b025
Thande Roy <39765073+thanderoy@users.noreply.github.com>
2023-03-11 17:56:51 +0300

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['thanderoy.com', 'thande-portfolio.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['thanderoy.com', 'thanderoy.up.railway.app', '127.0.0.1']
 
 
 # Application definition
